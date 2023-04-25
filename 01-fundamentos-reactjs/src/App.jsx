8896e6fd25ee9b52a3c6c10afd88261ce8fb537b
renyzeraa <renansilvaytb@gmail.com>
2023-04-25 20:19:52 -0300

    feat main App

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,18 +1,25 @@
 // named export evita problemas na importação dos arquivos
 import { Post } from './Post.jsx'
 import { Header } from './components/Header.jsx'
+import { Sidebar } from './components/Sidebar.jsx'
 
 import './global.css'
+import styles from './App.module.css'
 
 // named export
 export function App() {
     return (
         <>
             <Header />
-            <Post
-                author="Renan Silva"
-                content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quaerat eum ipsum soluta. Provident vitae numquam odit sapiente ipsam quas consequuntur facilis sunt. Facilis sunt commodi quaerat ea eius perspiciatis?"
-            />
+            <div className={styles.wrapper}>
+                <Sidebar></Sidebar>
+                <main>
+                    <Post
+                        author="Renan Silva"
+                        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quaerat eum ipsum soluta. Provident vitae numquam odit sapiente ipsam quas consequuntur facilis sunt. Facilis sunt commodi quaerat ea eius perspiciatis?"
+                    />
+                </main>
+            </div>
         </>
     )
 }
