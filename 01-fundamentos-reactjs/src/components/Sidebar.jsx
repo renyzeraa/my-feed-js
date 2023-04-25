21bcde368f8cc354bd1d8d04be6ad83955a59e89
renyzeraa <renansilvaytb@gmail.com>
2023-04-25 20:19:19 -0300

    feat sidebar

---
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import styles from './Sidebar.module.css'
+// import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.coverImg}
+                src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
+            <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src="https://github.com/renyzeraa.png"
+                />
+                <strong>Renan Silva</strong>
+                <span>Frontend Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    {/* <PencilLine />  */}
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
