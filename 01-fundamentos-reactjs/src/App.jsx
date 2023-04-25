71ca8f199b11a1713c3f3369461c520772796e0a
renyzeraa <renansilvaytb@gmail.com>
2023-04-25 18:44:46 -0300

    feat estilos globais

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,14 @@
 // named export evita problemas na importação dos arquivos
 import { Post } from './Post.jsx'
+import { Header } from './components/Header.jsx'
+
+import './global.css'
 
 // named export
 export function App() {
     return (
         <>
+            <Header />
             <Post
                 author="Renan Silva"
                 content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quaerat eum ipsum soluta. Provident vitae numquam odit sapiente ipsam quas consequuntur facilis sunt. Facilis sunt commodi quaerat ea eius perspiciatis?"
