f8e5800e14c38449017d77d3109f84e240ae5129
renyzeraa <renansilvaytb@gmail.com>
2023-04-26 18:08:26 -0300

    feat comment component

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsDown, ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/renyzeraa.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Renan Silva</strong>
+                            <time
+                                title="25 de Março de 2023 ás 12:15h"
+                                dataTime="2023-03-25 12:15:44"
+                            >
+                                Cerca de 1hr atrás
+                            </time>
+                        </div>
+                        <button title="Remover comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom dev !</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp /> Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import styles from './Post.module.css'
+import { Comment } from './Comment'
 
 export function Post() {
     return (
@@ -47,6 +48,11 @@ export function Post() {
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
         </article>
     )
 }
